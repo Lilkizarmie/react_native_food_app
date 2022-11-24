6a1e942ed424e2693f55b60d33a5e3d9123c7d39
Lilkizarmie <jefferyadolor18@gmail.com>
2022-11-24 02:44:48 +0100

    Bookmark implementation

---
+++ src/screens/BookmarkScreen.js
@@ -0,0 +1,99 @@
+import React from 'react';
+import {View, Text, StyleSheet, StatusBar, FlatList} from 'react-native';
+import {Colors, Fonts} from '../contants';
+import {BookmarkCard, Separator} from '../components';
+import {Display} from '../utils';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import {useSelector} from 'react-redux';
+import { useFonts } from 'expo-font';
+
+const ListItemSeparator = () => (
+  <View
+    style={{
+      height: 0.8,
+      backgroundColor: Colors.DEFAULT_GREY,
+      width: '100%',
+      marginVertical: 10,
+    }}
+  />
+);
+
+const BookmarkScreen = ({navigation}) => {
+  const bookmarks = useSelector(state => state?.bookmarkState?.bookmarks);
+  const [fontsLoaded] = useFonts({
+    'Poppins Black': require('../assets/fonts/Poppins-Black.ttf'),
+    'Poppins Bold': require('../assets/fonts/Poppins-Bold.ttf'),
+    'Poppins Extra Bold': require('../assets/fonts/Poppins-ExtraBold.ttf'),
+    'Poppins Extra Light': require('../assets/fonts/Poppins-ExtraLight.ttf'),
+    'Poppins Light': require('../assets/fonts/Poppins-Light.ttf'),
+    'Poppins Medium': require('../assets/fonts/Poppins-Medium.ttf'),
+    'Poppins Regular': require('../assets/fonts/Poppins-Regular.ttf'),
+    'Poppins Semi Bold': require('../assets/fonts/Poppins-SemiBold.ttf'),
+    'PoppinsThin': require('../assets/fonts/Poppins Thin.ttf'),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+  
+  return (
+    <View style={styles.container}>
+      <StatusBar
+        barStyle="dark-content"
+        backgroundColor={Colors.DEFAULT_WHITE}
+        translucent
+      />
+      <Separator height={StatusBar.currentHeight} />
+      <View style={styles.headerContainer}>
+        <Ionicons
+          name="chevron-back-outline"
+          size={30}
+          onPress={() => navigation.goBack()}
+        />
+        <Text style={styles.headerTitle}>Bookmarks</Text>
+      </View>
+      <FlatList
+        style={styles.bookmarkList}
+        data={bookmarks}
+        keyExtractor={item => item?.restaurantId}
+        showsVerticalScrollIndicator={false}
+        ListHeaderComponent={() => <Separator height={10} />}
+        ListFooterComponent={() => <Separator height={10} />}
+        ItemSeparatorComponent={() => <ListItemSeparator />}
+        renderItem={({item}) => (
+          <BookmarkCard
+            {...item?.restaurant}
+            navigate={restaurantId =>
+              navigation.navigate('Restaurant', {restaurantId})
+            }
+          />
+        )}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.DEFAULT_WHITE,
+  },
+  headerContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+  },
+  headerTitle: {
+    fontSize: 20,
+    fontFamily: 'Poppins Medium',
+    lineHeight: 20 * 1.4,
+    width: Display.setWidth(80),
+    textAlign: 'center',
+  },
+  bookmarkList: {
+    marginHorizontal: 20,
+  },
+});
+
+export default BookmarkScreen;
